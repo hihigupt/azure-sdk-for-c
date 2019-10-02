3f8044606aa7bac0a412c713910a7c18135f278f
Anton Kolesnyk <antk@microsoft.com>
2019-10-01 18:54:50 -0700

    Add a bit more structure

--- sdk/core/core/inc/az_login.h
+++ sdk/core/core/inc/az_login.h
@@ -7,6 +7,8 @@
 #include <az_result.h>
 #include <az_span.h>
 
+#include <cstddef.h>
+
 #include <_az_cfg_prefix.h>
 
 typedef enum {
@@ -25,7 +27,25 @@ typedef struct {
   } data;
 } az_login_info;
 
-az_result az_login(az_login_info const * const p_login_info);
+static inline az_result az_login_info_init_username_and_password(
+    az_const_span const username,
+    az_const_span const password,
+    az_login_info * const out_login_info) {
+  if (username == NULL || password == NULL || out_login_info == NULL) {
+    return AZ_ERROR_ARG;
+  }
+
+  *out_login_info = { .kind = AZ_LOGIN_INFO_KIND_USERNAME_AND_PASSWORD,
+                      .data = { .username = username, .password = password } };
+
+  return AZ_OK;
+}
+
+typedef struct {
+} az_cookie;
+
+az_result az_login(az_login_info const * const login_info, az_cookie * const out_cookie);
+az_result az_logout(az_cookie const * const cookie);
 
 #include <_az_cfg_suffix.h>
 
--- sdk/core/core/src/az_login.c
+++ sdk/core/core/src/az_login.c
@@ -3,14 +3,16 @@
 
 #include <az_login.h>
 
+#include <cstddef.h>
+
 #include <_az_cfg_warn.h>
 
-az_result az_login(az_login_info const * const p_login_info) {
-  if (p_login_info == NULL) {
-    return AZ_LOGIN_ERROR_INPUT_PARAM;
+az_result az_login(az_login_info const * const login_info, az_cookie * const out_cookie) {
+  if (login_info == NULL) {
+    return AZ_ERROR_ARG;
   }
 
-  switch (p_login_info->kind) {
+  switch (login_info->kind) {
     case AZ_LOGIN_INFO_KIND_USERNAME_AND_PASSWORD:
       return AZ_ERROR_NOT_IMPLEMENTED;
     default:
@@ -18,4 +20,12 @@ az_result az_login(az_login_info const * const p_login_info) {
   }
 
   return AZ_OK;
+}
+
+az_result az_logout(az_cookie const * const out_cookie) {
+  if (out_cookie == NULL) {
+    return AZ_ERROR_ARG;
+  }
+
+  return AZ_ERROR_NOT_IMPLEMENTED;
 }
