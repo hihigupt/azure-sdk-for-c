53cccbbb3ac44cc5e791888f31acf5663ca4e47f
Dane Walton <dawalton@microsoft.com>
2020-02-21 11:00:51 -0800

    basic telemetry working

---
+++ sdk/iot/samples/iot_core/iot_hub_telemetry.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include "az_span.h"
+#include "az_iot_hub_telemetry.h"
+#include "az_iot_hub_connect.h"
+
+#define DEVICE_ID "my_test_device"
+
+static uint8_t topic_buffer[100];
+static az_span props = AZ_SPAN_LITERAL_FROM_STR("key=value");
+static az_iot_identity id = {
+    ._internal.device_id = AZ_SPAN_LITERAL_FROM_STR(DEVICE_ID)
+};
+
+int main(void)
+{
+    az_span out_topic = AZ_SPAN_FROM_BUFFER(topic_buffer);
+    az_iot_telemetry_publish_topic_get(id, props, &out_topic);
+    printf("%.*s\r\n", az_span_length(out_topic), az_span_ptr(out_topic));
+}
+
---
+++ sdk/iot/src/az_iot_hub_telemetry.c
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "az_iot_hub_telemetry.h"
+
+static az_span telemetry_prop_delim = AZ_SPAN_LITERAL_FROM_STR("?");
+static az_span telemetry_topic_prefix = AZ_SPAN_LITERAL_FROM_STR("devices/");
+static az_span telemetry_topic_suffix = AZ_SPAN_LITERAL_FROM_STR("/messages/events/");
+
+
+az_result az_iot_telemetry_publish_topic_get(az_iot_identity identity, az_span properties, 
+                                                        az_span *out_mqtt_topic)
+{
+    az_result result;
+
+    if(az_span_capacity(*out_mqtt_topic) <
+            az_span_length(identity._internal.device_id) +
+            az_span_length(identity._internal.module_id) +
+            az_span_length(properties) + 
+            az_span_length(telemetry_topic_prefix) +
+            az_span_length(telemetry_topic_suffix))
+    {
+        result = AZ_ERROR_OUT_OF_MEMORY;
+    }
+    else
+    {
+        if((result = az_span_append(*out_mqtt_topic, telemetry_topic_prefix, out_mqtt_topic)) == AZ_OK)
+        {
+            if((result = az_span_append(*out_mqtt_topic, identity._internal.device_id, out_mqtt_topic)) == AZ_OK)
+            {
+                if((result = az_span_append(*out_mqtt_topic, telemetry_topic_suffix, out_mqtt_topic)) == AZ_OK)
+                {
+                    if((result = az_span_append(*out_mqtt_topic, telemetry_prop_delim, out_mqtt_topic)) == AZ_OK)
+                    {
+                        if((result = az_span_append(*out_mqtt_topic, properties, out_mqtt_topic)) == AZ_OK)
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+
+    return result;
+}
